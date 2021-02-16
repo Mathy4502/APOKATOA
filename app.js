7ba69afa7a0091b7dd0099179493864adc36c4c9
Mathy4502 <mathy4502@gmail.com>
2021-02-16 18:00:42 +0100

    fix merging probleme

--- app.js
+++ app.js
@@ -14,46 +14,20 @@ app.get("/", (req, res) => {
   return res.render("index.ejs");
 });
 
-<<<<<<< HEAD
-app.get('/about-us', (req, res) => {
-    return res.render('about-us.ejs');
-});
-
-app.get('/contact', (req, res) => {
-    return res.render('contact.ejs');
-});
-
-
-app.get('/programmation', (req, res) => {
-    return res.render('programmation.ejs');
-});
-
-app.get('/billeterie', (req, res) => {
-    return res.render('ticket.ejs');
-=======
 app.get("/about-us", (req, res) => {
   return res.render("about-us.ejs");
-});
-
-app.get("/blog", (req, res) => {
-  return res.render("blog.ejs");
 });
 
 app.get("/contact", (req, res) => {
   return res.render("contact.ejs");
 });
 
-app.get("/element", (req, res) => {
-  return res.render("elements.ejs");
-});
-
 app.get("/programmation", (req, res) => {
   return res.render("programmation.ejs");
 });
 
-app.get("/ticket", (req, res) => {
+app.get("/billeterie", (req, res) => {
   return res.render("ticket.ejs");
->>>>>>> dev
 });
 
 app.listen(3000);
