e56d8723c9a81afa290aa5dbcbf058f044d3e839
Mathy4502 <mathy4502@gmail.com>
2021-02-16 17:52:53 +0100

    modif routes

--- app.js
+++ app.js
@@ -1,43 +1,41 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 /* Parse POST request to json */
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //ejs
-app.set('views', './views')
-app.set('view engine', 'ejs')
+app.set("views", "./views");
+app.set("view engine", "ejs");
 
-
-app.get('/', (req, res) => {
-    return res.render('index.ejs');
+app.get("/", (req, res) => {
+  return res.render("index.ejs");
 });
 
-app.get('/', (req, res) => {
-    return res.render('about-us.ejs');
+app.get("/about-us", (req, res) => {
+  return res.render("about-us.ejs");
 });
 
-app.get('/', (req, res) => {
-    return res.render('blog.ejs');
+app.get("/blog", (req, res) => {
+  return res.render("blog.ejs");
 });
 
-app.get('/', (req, res) => {
-    return res.render('contact.ejs');
+app.get("/contact", (req, res) => {
+  return res.render("contact.ejs");
 });
 
-app.get('/', (req, res) => {
-    return res.render('elements.ejs');
+app.get("/element", (req, res) => {
+  return res.render("elements.ejs");
 });
 
-app.get('/', (req, res) => {
-    return res.render('programmation.ejs');
+app.get("/programmation", (req, res) => {
+  return res.render("programmation.ejs");
 });
 
-app.get('/', (req, res) => {
-    return res.render('ticket.ejs');
+app.get("/ticket", (req, res) => {
+  return res.render("ticket.ejs");
 });
-
 
 app.listen(3000);
